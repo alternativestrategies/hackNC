4bf2b60db05948dbab02ee24fd185c1e48a8f7f2
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-10-12 17:47:21 -0400

    Added skeleton to profile page

--- client/src/Components/Profile/Index.jsx
+++ client/src/Components/Profile/Index.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <p>Profile page</p>
+        <div>
+            <h2>Username:</h2>
+            <h2>Full Name:</h2>
+            <h2>Email:</h2>
+            <div>
+                <h3>Bid history</h3>
+                <table>
+                    <th>Product Name</th>
+                    <th>Bid Amount</th>
+                    <th>Bid Status</th>
+                    <tr>
+                        <td>Name#1</td>
+                        <td>$0.00</td>
+                        <td>Sold</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     )
 }
 
