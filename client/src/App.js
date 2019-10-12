f1155f69457560be9bf44194d7379860081acbd1
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-10-12 18:55:02 -0400

    Added modal module

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import ReactModalLogin from "react-modal-login";
 import Nav from './Components/Nav/Index';
 import Footer from './Components/Footer/Index';
 import Login from './Components/Login';
@@ -8,14 +9,82 @@ import Signup from './Components/Signup';
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      showModal: false,
+      loading: false,
+      error: null
+    }
+    console.log(this.state)
   }
+
+  openModal() {
+    this.setState({
+      showModal: true
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      showModal: false,
+      error: null
+    });
+  }
+
+  onLoginSuccess(method, response) {
+    console.log("logged successfully with " + method);
+  }
+
+  onLoginFail(method, response) {
+    console.log("logging failed with " + method);
+    this.setState({
+      error: response
+    });
+  }
+
+  startLoading() {
+    this.setState({
+      loading: true
+    });
+  }
+
+  finishLoading() {
+    this.setState({
+      loading: false
+    });
+  }
+
+  afterTabsChange() {
+    this.setState({
+      error: null
+    });
+  }
+
+  componentDidMount() {
+    this.openModal()
+  }
+
   render() {
     return (
       <React.Fragment >
         <Nav />
-        <Login />
-        <Signup />
+
+        <ReactModalLogin
+          visible={this.state.showModal}
+          onCloseModal={this.closeModal.bind(this)}
+          loading={this.state.loading}
+          error={this.state.error}
+          tabs={{
+            afterChange: this.afterTabsChange.bind(this)
+          }}
+          loginError={{
+            label: "Couldn't sign in, please try again."
+          }}
+          registerError={{
+            label: "Couldn't sign up, please try again."
+          }}
+          startLoading={this.startLoading.bind(this)}
+          finishLoading={this.finishLoading.bind(this)} />
         <Footer />
       </React.Fragment >
     );
