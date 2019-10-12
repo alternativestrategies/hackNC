e89b099ad61210ed3b8cbcc12a8be9b9bcd86c21
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-10-12 17:54:08 -0400

    Turned App.js into a class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Nav from './Components/Nav/Index';
@@ -6,15 +6,20 @@ import Footer from './Components/Footer/Index';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Nav />
-      <Login />
-      <Signup />
-      <Footer />
-    </React.Fragment>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <React.Fragment >
+        <Nav />
+        <Login />
+        <Signup />
+        <Footer />
+      </React.Fragment >
+    );
+  }
 }
 
 export default App;
