8d9758f9e04c191ca062dac589ff1ef490d4bf19
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-13 09:04:39 -0400

    change class to classname

--- client/src/Components/Home/Content.js
+++ client/src/Components/Home/Content.js
@@ -5,28 +5,28 @@ const Content = () => {
   return (
     <section id="who">
 
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
             <h2>Stars</h2>
 
             <hr align="left" width="50%" />
-            <p class="lead">
+            <p className="lead">
               Everyone knows the phrase "I'll get you the stars from the sky".
               Know it can finally come true ! Only Constellation Stars make it
               even more personal. Why not match the special occasion to an
               astrological date or a loved one’s horoscope!
             </p>
             <NavLink to="/products">
-            <button type="button" class="btn btn-who">
+            <button type="button" className="btn btn-who">
               Check it Out
             </button>
             </NavLink>
           </div>
 
-          <div class="col-md-6">
+          <div className="col-md-6">
             <img
-              class="img-fluid rounded-pill"
+              className="img-fluid rounded-pill"
               src="https://cdn.dribbble.com/users/1781642/screenshots/4821721/dribbble.jpg"
               alt="space"
             />
--- client/src/Components/Home/Sl.js
+++ client/src/Components/Home/Sl.js
@@ -4,58 +4,58 @@ const Sl = () => {
   return (
     <div
       id="carouselExampleIndicators"
-      class="carousel slide"
+      className="carousel slide"
       data-ride="carousel"
     >
-      <ol class="carousel-indicators">
+      <ol className="carousel-indicators">
         <li
           data-target="#carouselExampleIndicators"
           data-slide-to="0"
-          class="active"
+          className="active"
         ></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
       </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
           <img
-            class="d-block w-100"
+            className="d-block w-100"
             src="https://cdn.dribbble.com/users/477873/screenshots/2157764/galaxy_dribbble.jpg"
             alt="First slide"
           />
         </div>
-        <div class="carousel-item">
+        <div className="carousel-item">
           <img
-            class="d-block w-100"
+            className="d-block w-100"
             src="https://cdn.dribbble.com/users/2290824/screenshots/5518732/galaxy.png"
             alt="Second slide"
           />
         </div>
-        <div class="carousel-item">
+        <div className="carousel-item">
           <img
-            class="d-block w-100"
+            className="d-block w-100"
             src="https://cdn.dribbble.com/users/69890/screenshots/5515073/dribbble-galaxy-800x600.png"
             alt="Third slide"
           />
         </div>
       </div>
       <a
-        class="carousel-control-prev"
+        className="carousel-control-prev"
         href="#carouselExampleIndicators"
         role="button"
         data-slide="prev"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
       </a>
       <a
-        class="carousel-control-next"
+        className="carousel-control-next"
         href="#carouselExampleIndicators"
         role="button"
         data-slide="next"
       >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
       </a>
     </div>
   );
