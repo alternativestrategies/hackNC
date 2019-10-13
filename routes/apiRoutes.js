7eb25c8518d53da61b0c29381c50fcf0ba4390a2
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-13 00:09:17 -0400

    Add API endpoints and bcrypt and joi

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router()
 const Products = require('../models/products.js')
+const bcrypt = require('bcrypt')
+const joi = require('joi')
+
+
 
 router.get('/', (req, res) => {
     res.send('welcome to the api')
@@ -13,6 +17,44 @@ router.get('/products', async (req, res) => {
     } catch(err) {
         res.status(500).json({message: err.message})
     }
+})
+
+router.get('/products/filter/:galaxy', async (req, res) => {
+    const galaxy = req.params.galaxy;
+    const ANY = "any";
+    if(galaxy !== ANY){
+        try {
+            const products = await Products.find({galaxy: `${galaxy}`})
+            res.json(products)
+        }catch(err){
+            res.status(500).json({message: err.message})
+        }
+    }
+})
+
+router.put('/products/bid', async (req, res) => {
+    const newbid = req.body.bid;
+    const id = req.body.id
+    try{
+        const products = await Bid.update({_id: id}, {$max: {bid: newbid}})
+    }catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+router.post('/user/signup', async (req, res) => {
+    let user = await user.findOne({email: req.body.email});
+    if(user) return res.status(400).send('User already registered');
+
+    user = new user({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    await user.save()
+
+    res.send(user)
 })
 
 module.exports = router;
